b4ec265180f9fba0d21f394f509162fffa6eadd2
build0x <pengrenyou04@gmail.com>
2025-11-02 00:18:39 +0800

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repositoryName = "Felis";
+
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "export",
+  basePath: isProd ? `/${repositoryName}` : "",
+  assetPrefix: isProd ? `/${repositoryName}/` : undefined,
   images: {
     unoptimized: true,
   },
